5ea82ea0df15cf98860db432c32c6de6e7d0b8c5
Marina Nazina <nzyame@gmail.com>
2025-11-05 19:26:50 +0000

    task 1

--- script.js
+++ script.js
@@ -1,7 +1,53 @@
+// 1. Створити масив чисел від 0 до 30
+
+// a) Через цикл for
+
+const numbers = [];
+
+for (let i = 0; i <= 30; i += 1) {
+	numbers.push(i);
+}
+
+console.log(numbers);
+
+// b) Через Array.from()
+
+const array1 = Array.from({ length: 31 }, (el, index) => index);
+
+console.log(array1);
+
+// c) Через .fill() + .map()
+
+const array2 = new Array(31).fill(0).map((_, index) => index);
+
+console.log(array2);
+
+// 2. Універсальні функції
+
+// a) Через цикл for
+
+function createRange(start, end) {
+	const array = [];
+
+	for (let i = start; i <= end; i += 1) {
+		array.push(i);
+	}
+
+	return array;
+}
+
+console.log(createRange(5, 25));
+
+// b) Через Array.from()
+
+const createRange1 = (start, end) =>
+	Array.from({ length: end - start + 1 }, (_, index) => start + index);
+
+console.log(createRange1(5, 25));
+
+// c) Через .fill() + .map()
+
 const createRangeFillMap = (start, end) =>
-  new Array(end - start + 1)
-    .fill(0)
-    .map((_, index) => start + index);
+	new Array(end - start + 1).fill(0).map((_, index) => start + index);
 
 console.log(createRangeFillMap(5, 25));
-console.log(createRangeFillMap(0, 29));
